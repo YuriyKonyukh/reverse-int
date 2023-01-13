02ac78b7de9e58f4e4f7e58ee55947ac9d4a34af
Yuriy <yuriykonyukh@gmail.com>
2023-01-13 21:25:59 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let new_str = Math.abs(n).toString();
+  let new_arr = new_str.split("");
+  let reverse_str = new_arr.reverse();
+  let result_str = reverse_str.join("");
+  return result_str;
 }
